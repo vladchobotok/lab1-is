44c06e082111aacc511a594a9a3c0468ee90b834
Inanity <stepanyukvova200@gmail.com>
2023-10-31 01:48:09 +0200

    add printing data

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -57,6 +57,7 @@ public class Main {
         thirdUser.addProperty(name, "Sophie");
         thirdUser.addLiteral(age, 28);
 
+
         Individual teenagers = ageGroupClass.createIndividual(ontNS + "teenagers");
         Individual adults = ageGroupClass.createIndividual(ontNS + "adults");
 
@@ -86,16 +87,52 @@ public class Main {
             thirdUser.addProperty(canCommunicateWith, secondUser);
         }
 
+        printUserInfo(firstUser);
+        printUserInfo(secondUser);
+        printUserInfo(thirdUser);
+
+        printUsersInAgeGroup(teenagers);
+        printUsersInAgeGroup(adults);
+
+        printMembersOfUserGroup(firstGroup);
+        printMembersOfUserGroup(secondGroup);
+
         try (OutputStream outputStream = new FileOutputStream("ontology.owl")) {
             ontology.write(outputStream, "RDF/XML-ABBREV");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
     public static boolean canCommunicate(Individual user1, Individual user2) {
         RDFNode ageGroup1 = user1.getPropertyValue(user1.getOntModel().getProperty(user1.getNameSpace() + "hasAgeGroup"));
         RDFNode ageGroup2 = user2.getPropertyValue(user2.getOntModel().getProperty(user2.getNameSpace() + "hasAgeGroup"));
         return ageGroup1.equals(ageGroup2);
+    }
+
+    public static void printUserInfo(Individual user) {
+        String name = user.getPropertyValue(user.getOntModel().getProperty(user.getNameSpace() + "name")).asLiteral().getString();
+        int age = user.getPropertyValue(user.getOntModel().getProperty(user.getNameSpace() + "age")).asLiteral().getInt();
+        String ageGroup = user.getPropertyValue(user.getOntModel().getProperty(user.getNameSpace() + "hasAgeGroup")).asResource().getLocalName();
+        System.out.println("User: " + name + ", Age: " + age + ", Age Group: " + ageGroup);
+    }
+
+    public static void printUsersInAgeGroup(Individual ageGroup) {
+        StmtIterator users = ageGroup.getOntModel().listStatements(null, ageGroup.getOntModel().getProperty(ageGroup.getNameSpace() + "hasAgeGroup"), ageGroup);
+        System.out.println("Users in " + ageGroup.getLocalName() + " age group:");
+        while (users.hasNext()) {
+            Statement stmt = users.next();
+            Individual user = stmt.getSubject().as(Individual.class);
+            System.out.println(user.getLocalName());
+        }
+    }
+
+    public static void printMembersOfUserGroup(Individual userGroup) {
+        StmtIterator members = userGroup.getOntModel().listStatements(null,
+                userGroup.getOntModel().getProperty(userGroup.getNameSpace() + "memberOf"), userGroup);
+        System.out.println("Members of " + userGroup.getLocalName() + ":");
+        while (members.hasNext()) {
+            Statement stmt = members.next();
+            System.out.println(stmt.getSubject().getLocalName());
+        }
     }
 }
